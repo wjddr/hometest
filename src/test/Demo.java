8ba1807bcfa61730b528b6ba8f31f8479b35197b
wj <wj@windows10.microdone.cn>
2019-09-22 22:33:36 +0800

    test

--- src/test/Demo.java
+++ src/test/Demo.java
@@ -1,9 +1,24 @@
 package test;
 
+import java.math.BigDecimal;
+
 public class Demo {
 
 	public static void main(String[] args) {  
-        fun(2,6);
+		float a = 1.0f - 0.9f;
+		float x = 0.9f;
+        Double b = (double) (0.7f - 0.6f);
+        System.out.println(a);
+        int c=Float.floatToIntBits(x);
+        System.out.println(Integer.toBinaryString(c));
+        System.out.println(b);
+        if (a == b) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+		
+        
     }  
   
 	public static int fun(int x,int y) {
